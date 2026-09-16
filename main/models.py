b68ac9d07f578a5113cdd43712fbbb4eb268b1f5
Adrian Nathanael <adrianpersonal677@gmail.com>
2026-09-16 15:20:42 +0700

    refactor: change model thumnail into char so user can input path to reached the thumnail

--- main/models.py
+++ main/models.py
@@ -33,7 +33,7 @@ class Award(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    thumbnail = models.URLField(max_length=255, blank=True, null=True)
+    thumbnail = models.CharField(max_length=255, blank=True, null=True)
     issuer = models.CharField(max_length=255, blank=True, null=True)
     date_received = models.DateField()
 
